0de8dd04e8c2676f401486f75d807cd00686382f
Frank Kessler <frankkessler@gmail.com>
2016-09-20 14:21:13 -0400

    Add properties to incontact token model

--- src/Models/IncontactToken.php
+++ src/Models/IncontactToken.php
@@ -5,6 +5,25 @@ namespace Frankkessler\Incontact\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * inContact Token Model to store incontact oauth tokens in a database via Eloquent
+ *
+ * @property integer $id
+ * @property string $access_token
+ * @property string $refresh_token
+ * @property string $instance_base_url
+ * @property string refresh_instance_url
+ * @property string scope
+ * @property integer agent_id
+ * @property integer team_id
+ * @property integer business_unit
+ * @property integer $user_id
+ * @property \DateTimeInterface $expires
+ * @property \DateTimeInterface $created_at
+ * @property \DateTimeInterface $updated_at
+ * @property \DateTimeInterface $deleted_at
+ */
+
 class IncontactToken extends Model
 {
     use SoftDeletes;
